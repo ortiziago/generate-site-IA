1457d14b7163c2b15cf89c510c38349416eb34b2
Iago Ortiz <iago.ortizpereira@gmail.com>
2026-05-16 23:33:17 -0300

    Corrigido o problema da responsividade no preview

--- src/js/devices-controller.js
+++ src/js/devices-controller.js
@@ -7,19 +7,16 @@ import {
 } from "./global-variables.js";
 
 function changeSize() {
-  if (iframePreview.classList.contains("mobile")) {
-    const mobile = document.querySelector(".mobile");
-    mobile.classList.remove("small", "medium", "large");
+  if (!iframePreview.classList.contains("mobile")) return;
+  const mobile = document.querySelector(".mobile");
+  mobile.classList.remove("small", "medium", "large");
 
-    if (state.valueResponsivo === 400) {
-      mobile.classList.add("small");
-    } else if (state.valueResponsivo == 500) {
-      mobile.classList.add("medium");
-    } else {
-      mobile.classList.add("large");
-    }
+  if (state.valueResponsivo == 400) {
+    mobile.classList.add("small");
+  } else if (state.valueResponsivo == 500) {
+    mobile.classList.add("medium");
   } else {
-    return;
+    mobile.classList.add("large");
   }
 }
 
@@ -30,12 +27,26 @@ btnDevice.forEach((btn) => {
     });
     responsiveOptions.classList.remove("active");
     btn.classList.toggle("active");
+
+    if (iframePreview.classList.contains("mobile")) {
+      const mobile = document.querySelector(".mobile");
+      mobile.classList.remove("small", "medium", "large");
+      iframePreview.classList.remove("mobile");
+    } else {
+      return;
+    }
   });
 });
 
 btnMobile.addEventListener("click", () => {
   responsiveOptions.classList.add("active");
   iframePreview.classList.add("mobile");
+  changeSize();
+});
+
+responsiveOptions.addEventListener("change", (e) => {
+  state.valueResponsivo = e.target.value;
+  changeSize();
 });
 
 changeSize();
