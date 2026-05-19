37a86c3ea3e8a678bc55e049d3d4d7422142d8e1
Iago Ortiz <iago.ortizpereira@gmail.com>
2026-05-18 23:58:43 -0300

    Corrigido endereço do BE

--- src/js/request-ia.js
+++ src/js/request-ia.js
@@ -33,18 +33,15 @@ async function generateCode() {
       btn.disabled = true;
     });
 
-    const response = await fetch(
-      "https://generate-site-ia.onrender.com/generate",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt: userPrompt,
-        }),
+    const response = await fetch("https://generate-site-ia.onrender.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+      body: JSON.stringify({
+        prompt: userPrompt,
+      }),
+    });
 
     if (!response.ok) {
       throw new Error("Erro na API");
